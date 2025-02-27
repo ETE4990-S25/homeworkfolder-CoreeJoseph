81fae0d18b4b5a7d750832a0aad85ab86bca5efb
CoreeJoseph <cajoseph@cpp.edu>
2025-02-26 16:43:55 -0800

    hw3 done

--- Homework/Homework1/data_classes.py
+++ Homework/Homework1/data_classes.py
@@ -19,5 +19,20 @@ class Student(Person):
         person_dict = super().to_dict()
         person_dict["Student ID"] = self.student_id
         return person_dict
-def dumper():
-    print("info has been dumped to json file :)")
+    
+def save_to_json(obj,filename):
+    with open('hw3.json', 'w') as json_file:
+        json.dump(obj.to_dict(), json_file, indent=4)
+
+def display_json(filename):
+    with open('hw3.json', 'r') as json_file:
+        data = json.load(json_file)
+        print(json.dumps(data, indent=4))
+
+person = Person("bobby joe", 30, "bobbyjoe@khbsv.com")
+student = Student("cindy joe", 20, "cindyjoe@ksjdbv.com", "S12345")
+
+save_to_json(person, 'person.json')
+save_to_json(student, 'student.json')
+display_json(filename ='hw3.json')
+display_json(filename ='hw3.json')
